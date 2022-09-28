d350114f98057ca00db6b1414a6944c1cc9644bd
Andrew Marinello <85416891+AmarJon@users.noreply.github.com>
2022-09-28 12:29:31 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ package password_generator;
 public class Main {
 	public static void main(String[] args) {
 		Characters c = new Characters();
+		System.out.println("-------Your New Password-------");
 		System.out.println(c.finalPassword());
+		System.out.println("-------------------------------");
 	}
 }
